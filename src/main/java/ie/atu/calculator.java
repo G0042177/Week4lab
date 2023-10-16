2822830d82c16194230a2367afaf22f2301149ba
G00421777@atu.ie <G004221777@atu.ie>
2023-10-16 10:32:27 +0100

    Added multiplication

--- src/main/java/ie/atu/calculator.java
+++ src/main/java/ie/atu/calculator.java
@@ -1,7 +1,36 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class calculator {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Addition");
+        add();
+        System.out.println("Multiplication");
+        multiple();
+
+    }
+
+    public static void add() {
+        System.out.println("Please your first  number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please your second number :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("The total is " + total);
+    }
+    public static void multiple() {
+        System.out.println("Please your first  number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please your second number :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("The total is " + total);
     }
 }
