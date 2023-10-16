a8670b588bb328edac5b7af8578f2bb9f22dd28c
G00421777@atu.ie <G004221777@atu.ie>
2023-10-16 10:18:32 +0100

    added new method called main

--- src/main/java/ie/atu/calculator.java
+++ src/main/java/ie/atu/calculator.java
@@ -1,7 +1,22 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class calculator {
     public static void main(String[] args) {
-        System.out.println("hello");
+        add();
+        System.out.println("Hello");
+    }
+
+    public static void add() {
+        System.out.println("Please your first  number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please your second number :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("The total is " + total);
     }
 }
